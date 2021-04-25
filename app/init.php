e19931b324bd431cc20a38fc338ec2df219a4cc7
Thomas Carter <thomasj.carter1996@gmail.com>
2021-04-26 00:47:57 +0100

    Init Request Scheme
    Made BASE_URL constant use REQUEST_SCHEME server variable.

--- app/init.php
+++ app/init.php
@@ -3,8 +3,6 @@
 define("BASE_ROOT", dirname(__DIR__));
 define("THEMES", BASE_ROOT . '/themes'. DIRECTORY_SEPARATOR);
 
-
-
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $coreFile = BASE_ROOT  . "/app/" . $class . ".php";
@@ -12,7 +10,7 @@ spl_autoload_register(function ($class) {
     if (file_exists($coreFile)) include $coreFile;
 });
 
-define('BASE_URL', "http://" . $_SERVER['HTTP_HOST'] . str_replace($_SERVER['DOCUMENT_ROOT'], "" , str_replace("\\", "/'", BASE_ROOT)));
+define('BASE_URL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . str_replace($_SERVER['DOCUMENT_ROOT'], '' , str_replace('\\', '/', BASE_ROOT)));
 define("PUBLIC_ROOT", BASE_URL . '/public');
 
 ?>
