28d810e959e4817b7a5221f70fc567004cca0000
Thomas Carter <thomasj.carter1996@gmail.com>
2021-04-25 23:29:43 +0100

    Default Theme Styling
    Added basic theme styling

--- themes/default/components/footer.php
+++ themes/default/components/footer.php
@@ -1,7 +1,14 @@
         </div>
 
         <div class="site-footer">
+            <div class="footer-top">
+                <div class="container footer-container">
 
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <p>Copyright &copy; <?php echo SITE_TITLE . ' ' . date('Y'); ?></p>
+            </div>
         </div>
     </div>
 </body>
--- themes/default/components/header.php
+++ themes/default/components/header.php
@@ -12,6 +12,18 @@
 <body>
     <div class="site-wrapper">
         <div class="site-header">
+            <div class="container header-container">
+                <div class="header-left">
+                    <div class="logo">
+                        <h1 class="logo-text"><?php echo SITE_TITLE; ?></h1>
+                    </div>
+                    <div class="slogan">
+                        <p><?php echo SITE_DESCRIPTION; ?></p>
+                    </div>
+                </div>
+                <div class="header-right">
 
+                </div>
+            </div>
         </div>
         <div class="content-wrap">
